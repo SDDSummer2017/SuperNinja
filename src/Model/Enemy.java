8b3e1b78668bdf989cb0a0f70e4ac75756c8f755
matlock78 <Matlock_white@yahoo.com>
2017-06-21 20:30:56 -0700

    testing out commit / Matlock

--- src/Model/Enemy.java
+++ src/Model/Enemy.java
@@ -6,7 +6,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
-import static Model.Nen.getImage;
 import View.GamePanel;
 
 public abstract class Enemy extends GameFigure {
@@ -18,60 +17,27 @@ public abstract class Enemy extends GameFigure {
     public Image movement;
     public Image neutral;
     public Image is_Thrown;
+    public Image throwImage;
+    public Image staticImage;
     
     public int dx;
     public int dy;
     public int updateCount = 0;
-    public final Random random;
+    final Image image;
+    public String state;
 
-    /**
-     *
-     * @param x
-     * @param y
-     * @param size
-     */
-    public Enemy(int x, int y, int size) {}
-    
-    public void shoot(){
-        double targetX = Main.gameData.marine.x + Main.gameData.marine.size/2;
-        double targetY = Main.gameData.marine.y;
-        if(random.nextBoolean())
-            Main.gameData.addEnemyBullet(super.x + super.size/2, super.y + super.size, targetX, targetY, Color.red);
-    
+    public Enemy(double x, double y, double size) {
+        super(x, y, size);
+        image = null;
     }
+
+    public void changeState(){}
     
     @Override
     public void render(Graphics g) {}
 
     @Override
-    public void update() {
-        // Random movement 
-        this.updateCount++;
-        if(this.updateCount == 15){
-            shoot();
-            dx = (random.nextBoolean()) ? 2:-2;
-            dy = (random.nextBoolean()) ? 2:-2;
-            this.updateCount = 0;
-        }
-        super.x += dx;
-        super.y += dy;
-
-        if (super.x + size > GamePanel.PWIDTH) {
-            dx = -dx;
-            super.x = GamePanel.PWIDTH - size;
-        } else if (super.x - size < 0) {
-            dx = -dx;
-            super.x = size;
-        }
-
-        if (super.y + size > GamePanel.PHEIGHT - 120) {
-            dy = -dy;
-            super.y = GamePanel.PHEIGHT - 120 - size;
-        } else if (super.y - size < 0) {
-            dy = -dy;
-            super.y = size;
-        }
-    }
+    public void update() {}
 
     @Override
     public Rectangle2D.Double getCollisionBox() {
--- src/Model/Rai.java
+++ src/Model/Rai.java
@@ -6,7 +6,10 @@
 package Model;
 
 import static Model.Nen.getImage;
+import Model.States.CombatState;
+import Model.States.Rai_States.Movement;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -16,14 +19,39 @@ public class Rai extends Enemy {
     
     public Rai(int x, int y, int size) {
         super(x, y, size);
+        super.state = "Neutral";
         this.health = 120;
         String imagePath = System.getProperty("user.dir");
         String separator = System.getProperty("file.separator");
         super.attack1 = getImage(imagePath + separator + "images" + separator
                 + "Rai_ViperStrike.png");
+        super.attack2 = getImage(imagePath + separator + "images" + separator
+                + "Rai_SteelTwister.png");
+        super.movement = getImage(imagePath + separator + "images" + separator
+                + "Rai_Movement.png");
+        super.block = getImage(imagePath + separator + "images" + separator
+                + "Rai_Block.png");
+        super.neutral = getImage(imagePath + separator + "images" + separator
+                + "Rai_Neutral.png");
+        super.throwImage = getImage(imagePath + separator + "images" + separator
+                + "Rai_Throw.png");
+        super.staticImage = getImage(imagePath + separator + "images" + separator
+                + "Rai_Static.png");
     }
     
     @Override
-    public void render(Graphics g) {}
+    public void render(Graphics g) {
+    if (super.cState instanceOf Movement){}
+    }
     
+    @Override
+    public void changeState(){}
+
+    @Override
+    public void update() {}
+
+    @Override
+    public Rectangle2D.Double getCollisionBox() {
+        return new Rectangle2D.Double(super.x, super.y, super.size, super.size);
+    }
 }
