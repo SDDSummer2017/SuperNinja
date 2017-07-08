12816818c01e8a993e78e5fc154aef18923591a7
abilby3 <abilby@uco.edu>
2017-07-08 18:54:46 -0500

    Fixed Branch with properties and classfiles

--- src/Model/GameFigure.java
+++ src/Model/GameFigure.java
@@ -6,7 +6,7 @@ import Model.States.MotionState;
 import Physics.Acceleration;
 import Physics.Force;
 import Physics.Velocity; 
-import StatusEffects.StatusEffects;
+import StatusEffects.EffectsManager;
 import java.awt.Image; 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D; 
@@ -33,7 +33,7 @@ public abstract class GameFigure implements Collision, Renderable, Updateable {
     public Point2D.Double location;
     public static final int GRAVITY = 8;
     public boolean hit;
-    public StatusEffects effectsManager;
+    public EffectsManager effectsManager;
     public HitBox hitbox;
     public boolean isGoodGuy;
     //Animation Attributes
@@ -59,7 +59,7 @@ public abstract class GameFigure implements Collision, Renderable, Updateable {
         this.location = new Point2D.Double(x - (size/2), y-(size/2));
         health = 100;
         this.isGoodGuy = isGoodGuy;
-        this.effectsManager = new StatusEffects();
+        this.effectsManager = new EffectsManager();
         this.moveFrameIndex = this.idleFrameIndex = this.idleFrameDelayCount = this.jumpFrameIndex = 0;
         this.moveLeftAnimation =  null;
         this.moveRightAnimation = null;
@@ -81,7 +81,7 @@ public abstract class GameFigure implements Collision, Renderable, Updateable {
         this.location = new Point2D.Double(x - (size/2), y-(size/2));
         health = 100;
         this.isGoodGuy = isGoodGuy;
-        this.effectsManager = new StatusEffects();
+        this.effectsManager = new EffectsManager();
         this.moveLeftAnimation =  new Image[mLength];
         this.moveRightAnimation = new Image[mLength];
         this.lightAttackLeftAnimation = new Image[arLength];
