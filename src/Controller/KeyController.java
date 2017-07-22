d09598538a252cb704d309d7fc0efa9daa56012b
abilby3 <abilby@uco.edu>
2017-07-22 18:14:35 -0500

    Revert "Mode keys reconfigurable..."
    This reverts commit 853bc8ffb3208565eb50d6f63c4ab51159fce505.

--- src/Controller/KeyController.java
+++ src/Controller/KeyController.java
@@ -1,59 +1,129 @@
-package Controller; 
+package Controller;
+
+import Model.Dummy;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import Model.Nen; 
-import View.KeyBindingWindow;
-import java.util.ArrayList;
-import java.util.HashMap; 
+import Model.Nen;
+import Model.States.Nen.Crouch;
+import Model.States.Nen.Jump;
+import Model.States.Nen.Move;
+import java.util.Random;
 
 
 public class KeyController implements KeyListener {
      
     Nen nen = Main.gameData.nen;
-    KeyBindingWindow window;
-    ArrayList<Command> keyCommands = new ArrayList<>(); 
-    
-    public KeyController(){ 
-        keyCommands.add(new LightAttackCommand(KeyEvent.VK_F));
-        keyCommands.add(new HeavyAttackCommand(KeyEvent.VK_D));
-        keyCommands.add(new MoveLeftCommand(KeyEvent.VK_LEFT));
-        keyCommands.add(new MoveRightCommand(KeyEvent.VK_RIGHT));
-        keyCommands.add(new JumpCommand(KeyEvent.VK_UP));
-        keyCommands.add(new WallJumpCommand(KeyEvent.VK_SPACE));
-        
-    }
+ 
+   
     @Override
     public void keyPressed(KeyEvent e) {   
-        
-            for(Command c : keyCommands) 
-                if(e.getKeyCode() == c.getKeyCode())
-                    c.execute(nen); 
+        switch (e.getKeyCode()){
+            case  KeyEvent.VK_LEFT:
+                    nen.isFacingRight = false;
+//                if(nen.mState.getClass() == Jump.class)
+//                {
+//                    boolean performWallJump = Main.gamePanel.checkWallRightJump();
+//                    if(performWallJump)
+//                        nen.mState.nextState("WallJump");     
+//                }
+                
+               if(nen.mState.getClass() != Move.class)
+                {
+
+                     nen.mState.nextState("Move");
+                }
+                
+                break;
+            case KeyEvent.VK_RIGHT:
+                nen.isFacingRight = true;
+                
+//                if(nen.mState.getClass() == Jump.class)
+//                {
+//                    boolean performWallJump = Main.gamePanel.checkWallLeftJump();
+//                    if(performWallJump)
+//                        nen.mState.nextState("WallJump");
+//                    
+//                        
+//                }
+                
+                if(nen.mState.getClass() != Move.class)
+                {
+                     nen.mState.nextState("Move");
+                }
+                break;
+            case KeyEvent.VK_UP:
+                if(nen.mState.getClass() != Jump.class)
+                {
+                     nen.mState.nextState("Jump");
+                }
+                break;
+            
+            case KeyEvent.VK_DOWN:
+                if(nen.mState.getClass() != Crouch.class)
+                {
+                     nen.mState.nextState("Crouch");
+                }
+                break;
+            case KeyEvent.VK_S: 
+                    Random rand = new Random();
+                    int  n = rand.nextInt(500) + 1;
+                    Main.gameData.addGameData(new Dummy(n, n, 25)); 
+                
+                break;
+                
+            case KeyEvent.VK_F:
+                if(nen.cState.getClass() != Model.States.Nen.LightAttack.class)
+                {
+                     nen.cState.nextState("LightAttack");
+                }
+               
+                break;
+            case KeyEvent.VK_D:
+                if(nen.cState.getClass() != Model.States.Nen.LightAttack.class)
+                {
+                     nen.cState.nextState("HeavyAttack");
+                }
+                break;
+            case KeyEvent.VK_SPACE:
+                if(nen.mState.getClass() == Jump.class && nen.isFacingRight)
+                {
+                    boolean performWallJump = Main.gamePanel.checkWallLeftJump();
+                    if(performWallJump)
+                        nen.mState.nextState("WallJump");
+                    
+                        
+                }else if(nen.mState.getClass() == Jump.class && !nen.isFacingRight)
+                {
+                    boolean performWallJump = Main.gamePanel.checkWallRightJump();
+                    if(performWallJump)
+                        nen.mState.nextState("WallJump"); 
+                }
+                break;
+        }
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
         if(e.getKeyChar() == 'f' || e.getKeyChar() == 'F')
-                nen.cState.nextState("LightAttack"); 
+                nen.cState.nextState("LightAttack");
+        
     }
-    
 
     @Override
     public void keyReleased(KeyEvent e) {
-        for(Command c : keyCommands)
-            c.release(nen);
-        
         switch (e.getKeyCode()){
- 
-            case KeyEvent.VK_ESCAPE:
-                if(window == null)
-                    window = new KeyBindingWindow(this);
-                else
-                    window.setVisible(!window.isVisible()); 
+            case  KeyEvent.VK_LEFT:
+                nen.mState.nextState("NeutralMotion");
+                break;
+            case KeyEvent.VK_RIGHT:
+                nen.mState.nextState("NeutralMotion");
+                break;        
+            case KeyEvent.VK_DOWN:
+                nen.mState.nextState("NeutralMotion");
+                break;
+            case KeyEvent.VK_Q:
+                Main.gamePanel.show = !Main.gamePanel.show;
+                break;
         }
-    } 
-    
-     public void rebindKeys(HashMap<Integer, Integer> newKeyCodes){
-         for(Command c : keyCommands)  
-             c.setKeyCode(newKeyCodes.get(c.keyCode));
     }
 }
