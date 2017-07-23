02a43f6ee75b51d7533c889e8a29bd5554939ac7
abilby3 <abilby@uco.edu>
2017-07-22 19:12:59 -0500

    Revert "Revert "Mode keys reconfigurable...""
    This reverts commit d09598538a252cb704d309d7fc0efa9daa56012b.

---
+++ src/Controller/JumpCommand.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.GameFigure;
+
+/**
+ *
+ * @author Garrett A. Clement
+ */
+public class JumpCommand extends Command {
+
+    public JumpCommand(int keyCode) {
+        super(keyCode);
+    }
+
+    
+    
+    @Override
+    public void execute(GameFigure gameFigure) { 
+        gameFigure.mState.nextState("Jump");
+    }
+
+}
